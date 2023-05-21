2442f344c088f1e7c733b1fd6e0ef8e96e37d9f0
DavidMes17 <davidmes46@gmail.com>
2023-05-21 19:46:02 -0300

    arquivos revisados

--- exer04.c
+++ exer04.c
@@ -5,11 +5,9 @@
 int main(void){
 setlocale(LC_ALL, "pt_BR");
 time_t currentTime;
-struct tm *LocalTime;
+struct tm *localTime;
+
     //faça um programa que receba o ano de nascimento de uma pessoa e o ano atual, calcule e mostre a idade da pessoa em anos, meses e dias
-    int ano, dia;
-    char mes[20];
-    int mesInt = atoi(mes);
 
     // Obtém o tempo atual
     currentTime = time(NULL);
@@ -17,16 +15,27 @@ struct tm *LocalTime;
     // Obtém a hora local
     localTime = localtime(&currentTime);
 
-    // Obtém o nome mês atual
+    // Obtém o nome do mês atual
+    char mes[20];
     strftime(mes, sizeof(mes), "%b", localTime);
 
     // Obtém o dia atual
-    dia = localTime->tm_mday
+    int dia = localTime->tm_mday;
 
     printf("Digite o seu ano de nascimento: ");
-        scanf("%d", &ano);
+    int ano;
+    scanf("%d", &ano);
 
-    mes = 12 - mesInt;
+    // Cálculo da idade em anos
+    int idade = localTime->tm_year + 1900 - ano;
+
+    // Cálculo da idade em meses
+    int meses = localTime->tm_mon + 1;
+
+    // Cálculo da idade em dias
+    int dias = localTime->tm_mday;
+
+    printf("Idade: %d anos, %d meses, %d dias\n", idade, meses, dias);
     
 
     return 0;
--- exer09.c
+++ exer09.c
@@ -3,8 +3,25 @@
 
 int main(void){
 setlocale(LC_ALL, "pt_BR");
-    //faça um programa que recea 2 notas, calcule e mostre a média ponderada dessas notas, considerando peso 2 para a primeira nota e peso 3 para a segunda nota
-    
+    //faça um programa que receba 2 notas, calcule e mostre a média ponderada dessas notas, considerando peso 2 para a primeira nota e peso 3 para a segunda nota
+    float nota1, nota2;
+    float peso1 = 2.0, peso2 = 3.0;
+    float mediaPonderada;
+
+    do{
+            printf("Digite o valor da primeira nota entre 0 e 10: ");
+        scanf("%f", &nota1);
+} while (nota1 < 0 || nota1 > 10);
+do{
+    printf("Digite o valor da segunda nota entre 0 e 10: ");
+        scanf("%f", &nota2);
+} while (nota2 < 0 || nota2 > 10);
+
+
+
+    mediaPonderada = (nota1 * peso1 + nota2 * peso2) / (peso1 + peso2);
+
+    printf("A média ponderada das notas é: %.2f", mediaPonderada);
     
     return 0;
 }
